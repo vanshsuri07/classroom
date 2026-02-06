49dc76198e57d416da4593b5a6a4bf7375214416
Vanshsuri07 <vanshsuri07@gmail.com>
2026-02-06 19:57:03 +0530

    fix: use single backend url

--- classroom-frontend/src/constants/index.ts
+++ classroom-frontend/src/constants/index.ts
@@ -67,9 +67,9 @@ const getEnvVar = (key: string): string => {
 
 export const CLOUDINARY_UPLOAD_URL=getEnvVar("VITE_CLOUDINARY_UPLOAD_URL");
 export const CLOUDINARY_CLOUD_NAME = getEnvVar("VITE_CLOUDINARY_CLOUD_NAME");
-export const BACKEND_BASE_URL = getEnvVar("VITE_BACKEND_BASE_URL");
+export const BACKEND_BASE_URL = import.meta.env.VITE_BACKEND_BASE_URL || "http://localhost:8000";
 
-export const BASE_URL =  import.meta.env.VITE_API_URL;
+
 // export const ACCESS_TOKEN_KEY = import.meta.env.VITE_ACCESS_TOKEN_KEY
 // export const REFRESH_TOKEN_KEY = import.meta.env.VITE_REFRESH_TOKEN_KEY
 
